4ddb42f6ed94bdc715553e3ca13bb043cf2b9779
Jason van Hattum <jasonvhattum@gmail.com>
2017-05-17 00:30:17 +0200

    WAAAAAAAAAAAAAAAAAAAAAAAAA

--- app/src/main/java/com/example/android/another_titanium/ScheduleDialog.java
+++ app/src/main/java/com/example/android/another_titanium/ScheduleDialog.java
@@ -20,7 +20,8 @@ public class ScheduleDialog extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.DialogeTheme);
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
-        builder.setView(inflater.inflate(R.layout.fragment_schedule, null))
+        View v = inflater.inflate(R.layout.fragment_schedule, null);
+        builder.setView(v)
                 .setNegativeButton("Save", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
 
@@ -32,6 +33,25 @@ public class ScheduleDialog extends DialogFragment {
                     ScheduleDialog.this.getDialog().cancel();
                 }
         });
+
+        Spinner s1 = (Spinner)v.findViewById(R.id.TypeSpinner);
+        ArrayAdapter<CharSequence> a1 = ArrayAdapter.createFromResource(getContext(),
+                R.array.type, android.R.layout.simple_spinner_item);
+        a1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s1.setAdapter(a1);
+
+        Spinner s2 = (Spinner)v.findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> a2 = ArrayAdapter.createFromResource(getContext(),
+                R.array.PowerStates, android.R.layout.simple_spinner_item);
+        a2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s2.setAdapter(a2);
+
+        Spinner s3 = (Spinner)v.findViewById(R.id.times);
+        ArrayAdapter<CharSequence> a3 = ArrayAdapter.createFromResource(getContext(),
+                R.array.Time, android.R.layout.simple_spinner_item);
+        a3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s3.setAdapter(a3);
+
 
         return builder.create();
     }
